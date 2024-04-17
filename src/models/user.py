bd5c6469aa74972f1c52c52fa99b93976a566cd5
2jun0 <soo28819@gmail.com>
2024-04-18 02:51:51 +0900

    feat: implement logic for updating user's pokemons
    according to proper pokemon count a user adds new pokemons or resets pokemons

--- src/models/user.py
+++ src/models/user.py
@@ -15,15 +15,40 @@ class User(SQLModel, DatedAtMixin, table=True):
     id: int | None = Field(default=None, primary_key=True)
     username: str = Field(unique=True)
     commit_point: int
-    pokemons: list[Pokemon] = Relationship(sa_relationship_kwargs={"lazy": "joined"})
-
-    def __post_init__(self):
-        n_pokemon = ceil(self.commit_point / settings.POKEMON_PER_COMMIT_POINT)
-        pokemon_types = random.sample(list(PokemonType), n_pokemon)
-
-        for pt in pokemon_types:
-            self.pokemons.append(Pokemon(type=pt))
+    pokemons: list[Pokemon] = Relationship(sa_relationship_kwargs={"lazy": "joined", "cascade": "all, delete"})
 
     @property
     def pokemon_types(self) -> list[PokemonType]:
         return [pokemon.type for pokemon in self.pokemons]
+
+    def __post_init__(self):
+        self._init_pokemons()
+
+    def _init_pokemons(self):
+        n_pokemons = self._calc_proper_pokemon_cnt()
+        pokemon_types = random.sample(list(PokemonType), n_pokemons)
+
+        for pt in pokemon_types:
+            self.pokemons.append(Pokemon(type=pt))
+
+    def update_pokemons(self):
+        proper_n_pokemons = self._calc_proper_pokemon_cnt()
+
+        if len(self.pokemons) > proper_n_pokemons:
+            self._reset_pokemons()
+        elif len(self.pokemons) < proper_n_pokemons:
+            self._add_new_pokemons(proper_n_pokemons - len(self.pokemons))
+
+    def _reset_pokemons(self):
+        self.pokemons.clear()
+        self._init_pokemons()
+
+    def _add_new_pokemons(self, new_cnt: int):
+        remain_types = set(PokemonType) - set(self.pokemon_types)
+        pokemon_types = random.sample(list(remain_types), new_cnt)
+
+        for pt in pokemon_types:
+            self.pokemons.append(Pokemon(type=pt))
+
+    def _calc_proper_pokemon_cnt(self):
+        return ceil(self.commit_point / settings.POKEMON_PER_COMMIT_POINT)
