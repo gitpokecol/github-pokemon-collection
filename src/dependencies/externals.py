67174129a15401b588e3693d2780c22e5ac669b1
2jun0 <soo28819@gmail.com>
2024-04-18 14:02:41 +0900

    feat: impliment github api (detail is not implimented)

---
+++ src/dependencies/externals.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from ..external.github_api import GithubAPI
+
+
+async def get_github_api() -> GithubAPI:
+    return GithubAPI()
+
+
+GITHUB_API_DEP = Annotated[GithubAPI, Depends(get_github_api)]
---
+++ src/external/github_api.py
@@ -0,0 +1,3 @@
+class GithubAPI:
+    async def get_user_total_contributions(self, *, username: str) -> dict[int, int]:
+        return {2023: 1}
