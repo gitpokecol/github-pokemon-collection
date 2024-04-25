6c7b387b37cf611476b54db6f6043786d8a78c4f
2jun0 <soo28819@gmail.com>
2024-04-25 17:47:59 +0900

    refactor: base exception(custom) has http_status and combine handlers into one

--- src/exceptions/base.py
+++ src/exceptions/base.py
@@ -1,5 +1,9 @@
+from fastapi import status
+
+
 class BaseException(Exception):
     detail: str
+    http_status: int
 
     def __new__(cls):
         if cls is BaseException:
@@ -10,11 +14,14 @@ class BaseException(Exception):
 
 class NotFoundError(BaseException):
     detail = "Not Found Error"
+    http_status = status.HTTP_404_NOT_FOUND
 
 
 class BadRequestError(BaseException):
     detail = "Bad Request Error"
+    http_status = status.HTTP_400_BAD_REQUEST
 
 
 class InternalError(BaseException):
     detail = "Internal Server Error"
+    http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
--- src/exceptions/handler.py
+++ src/exceptions/handler.py
@@ -1,25 +1,11 @@
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.exception_handlers import http_exception_handler
 
-from src.exceptions.base import BadRequestError, InternalError, NotFoundError
+from src.exceptions.base import BaseException
 
 
 def install_exception_handlers(app: FastAPI):
-    @app.exception_handler(NotFoundError)
-    async def not_found_error_handler(request: Request, err: NotFoundError):
-        http_exc = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=err.detail)
+    @app.exception_handler(BaseException)
+    async def custom_exception_handler(request: Request, err: BaseException):
+        http_exc = HTTPException(status_code=err.http_status, detail=err.detail)
         return await http_exception_handler(request, http_exc)
-
-    @app.exception_handler(InternalError)
-    async def internal_server_error_handler(request: Request, err: InternalError):
-        http_exc = HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=err.detail)
-        return await http_exception_handler(request, http_exc)
-
-    @app.exception_handler(BadRequestError)
-    async def bad_request_error_handler(request: Request, err: BadRequestError):
-        http_exc = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=err.detail)
-        return await http_exception_handler(request, http_exc)
-
-    @app.exception_handler(Exception)
-    async def exception_handler(request: Request, err: Exception):
-        return await internal_server_error_handler(request, InternalError())
