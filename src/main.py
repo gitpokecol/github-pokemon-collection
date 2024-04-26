72c5e1916261c28894d2bcc87cb72e9a5107fcd6
2jun0 <soo28819@gmail.com>
2024-04-26 18:05:08 +0900

    fix: disable docs url on not dev / test but production

--- src/main.py
+++ src/main.py
@@ -5,9 +5,10 @@ from src.routes.pokemons import router as pokemon_router
 from src.setting import Envrionment, settings
 
 if settings.ENVIRONMENT is Envrionment.PRODUCTION:
-    app = FastAPI(title=settings.TITLE, version=settings.VERSION)
-else:
     app = FastAPI(title=settings.TITLE, version=settings.VERSION, docs_url=None, redoc_url=None)
+else:
+    app = FastAPI(title=settings.TITLE, version=settings.VERSION)
+
 
 app.include_router(pokemon_router)
 install_exception_handlers(app)
