22e5f8f791139e6e72af1163d52ae1099358c04e
2jun0 <soo28819@gmail.com>
2024-04-30 21:57:04 +0900

    tests: remove unused files

--- tests/integration/github_api.py
+++
@@ -1,9 +0,0 @@
-from tests.integration import value
-
-
-class GithubAPIFake:
-    async def get_user_total_contributions(self, *, username: str) -> dict[int, int]:
-        return value.user_total_contributions
-
-    async def get_user_contributions_by_year(self, *, username: str, year: int) -> int:
-        return value.user_total_contributions[year]
--- tests/integration/value.py
+++
@@ -1,8 +0,0 @@
-username = "2jun0"
-user_total_contributions = {
-    2021: 50,
-    2022: 200,
-    2023: 0,
-    2024: 100,
-}
-pokemon_cnt = 4
