b48a28cdaeeb6f74db703e7b940246a272a12626
2jun0 <soo28819@gmail.com>
2024-07-11 02:55:26 +0900

    feat: release connection after create user or update user data

--- src/services/user.py
+++ src/services/user.py
@@ -7,27 +7,25 @@ from src.models.user import User
 
 
 class UserService:
+
     async def create_new_user(self, *, session: AsyncSession, username: str, commit_points: dict[int, int]) -> User:
         user = User(username=username)
         user.set_commit_points(commit_points)
 
         session.add(user)
         await session.commit()
-        await session.refresh(user)
+        await session.close()
 
         return user
 
-    async def update_commit_point(self, *, session: AsyncSession, username: str, year: int, commit_point: int) -> User:
+    async def update_commit_point(self, *, session: AsyncSession, username: str, year: int, commit_point: int) -> None:
         user = await self.get_user(session, username)
         if user is None:
             raise NotFoundError(ErrorCode.USER_NOT_FOUND)
 
         user.set_commit_point(year, commit_point)
-
         await session.commit()
-        await session.refresh(user)
-
-        return user
+        await session.close()
 
     async def get_user(self, session: AsyncSession, username: str) -> User | None:
         stmt = select(User).where(User.username == username)
