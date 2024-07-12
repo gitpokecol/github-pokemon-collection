217a3318a81f126717e30a9d7ecd1b6a83d9b943
2jun0 <soo28819@gmail.com>
2024-07-13 00:27:00 +0900

    feat: use saved image files instead using requests

--- src/renders/images.py
+++ src/renders/images.py
@@ -1,98 +1,76 @@
 import base64
+from functools import lru_cache
+from typing import Literal
 
+import aiofiles
 from httpx import AsyncClient
 
 from src.models.pokemon_type import PokemonType
-from src.utils import file_cache
 
-SPRITE_BASE_URL = "https://www.pokencyclopedia.info/sprites/overworlds"
-POKEBALL_URL = "https://www.pokencyclopedia.info/sprites/items/items_old/i_old_poke-ball.png"
-
-pokemon_left_sprites: dict[PokemonType, tuple[str, str]] = {}
-pokeball: str = ""
+POKEBALL_IMAGE_PATH = "imgs/ui/pokeball.png"
 
 
 class ImageLoader:
     def __init__(self) -> None:
         self._client = AsyncClient(http2=True)
 
-    @file_cache("pokemon_left_sprites")
+    @lru_cache
     async def get_pokemon_left_sprites(self) -> dict[PokemonType, tuple[str, str]]:
-        return {pokemon: await self._create_left_sprite_urls(pokemon) for pokemon in PokemonType}
+        return {
+            pokemon_type: (
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "left", False, 1)),
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "left", False, 2)),
+            )
+            for pokemon_type in PokemonType
+        }
 
-    @file_cache("pokemon_right_sprites")
+    @lru_cache
     async def get_pokemon_right_sprites(self) -> dict[PokemonType, tuple[str, str]]:
-        return {pokemon: await self._create_right_sprite_urls(pokemon) for pokemon in PokemonType}
+        return {
+            pokemon_type: (
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "right", False, 1)),
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "right", False, 2)),
+            )
+            for pokemon_type in PokemonType
+        }
 
-    @file_cache("pokemon_shiny_left_sprites")
+    @lru_cache
     async def get_pokemon_shiny_left_sprites(self) -> dict[PokemonType, tuple[str, str]]:
-        return {pokemon: await self._create_shiny_left_sprite_urls(pokemon) for pokemon in PokemonType}
+        return {
+            pokemon_type: (
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "left", True, 1)),
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "left", True, 2)),
+            )
+            for pokemon_type in PokemonType
+        }
 
-    @file_cache("pokemon_shiny_right_sprites")
+    @lru_cache
     async def get_pokemon_shiny_right_sprites(self) -> dict[PokemonType, tuple[str, str]]:
-        return {pokemon: await self._create_shiny_right_sprite_urls(pokemon) for pokemon in PokemonType}
+        return {
+            pokemon_type: (
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "right", True, 1)),
+                await self._load_as_base64(self._get_pokemon_sprite_path(pokemon_type, "right", True, 2)),
+            )
+            for pokemon_type in PokemonType
+        }
 
-    @file_cache("pokeball")
+    @lru_cache
     async def get_pokeball(self) -> str:
-        return await self._get_as_base64(POKEBALL_URL)
+        return await self._load_as_base64(POKEBALL_IMAGE_PATH)
 
-    async def _get_as_base64(self, url: str) -> str:
-        res = await self._client.get(url)
-        res.raise_for_status()
-        return "data:" + res.headers["Content-Type"] + ";" + "base64," + base64.b64encode(res.content).decode("utf-8")
+    async def _load_as_base64(self, path: str) -> str:
+        async with aiofiles.open(path, "rb") as f:
+            content = await f.read()
+            return "data:image/png;base64," + base64.b64encode(content).decode("utf-8")
 
-    async def _create_left_sprite_urls(self, type: PokemonType) -> tuple[str, str]:
-        frame_1, frame_2 = SpriteUrl(SPRITE_BASE_URL).left().type(type).frames()
-        return (await self._get_as_base64(frame_1), await self._get_as_base64(frame_2))
-
-    async def _create_right_sprite_urls(self, type: PokemonType) -> tuple[str, str]:
-        frame_1, frame_2 = SpriteUrl(SPRITE_BASE_URL).right().type(type).frames()
-        return (await self._get_as_base64(frame_1), await self._get_as_base64(frame_2))
-
-    async def _create_shiny_left_sprite_urls(self, type: PokemonType) -> tuple[str, str]:
-        frame_1, frame_2 = SpriteUrl(SPRITE_BASE_URL).left_shiny().type(type).frames()
-        return (await self._get_as_base64(frame_1), await self._get_as_base64(frame_2))
-
-    async def _create_shiny_right_sprite_urls(self, type: PokemonType) -> tuple[str, str]:
-        frame_1, frame_2 = SpriteUrl(SPRITE_BASE_URL).right_shiny().type(type).frames()
-        return (await self._get_as_base64(frame_1), await self._get_as_base64(frame_2))
-
-
-class SpriteUrl:
-    def __init__(self, base: str) -> None:
-        """Will set `{base}/{face}{type}{frame}.png`"""
-
-        self._base = base
-        self._face = None
-        self._type = None
-
-    def right(self):
-        self._face = "o-r_hgss/o-r_hs_"
-        return self
-
-    def left(self):
-        self._face = "o-l_hgss/o-l_hs_"
-        return self
-
-    def right_shiny(self):
-        self._face = "o-r_hgss_shiny/o-r_hs-S_"
-        return self
-
-    def left_shiny(self):
-        self._face = "o-l_hgss_shiny/o-l_hs-S_"
-        return self
-
-    def type(self, poke_type: PokemonType):
-        self._type = str(poke_type.national_no).zfill(3)
-        if poke_type.national_no in [3, 25]:
-            self._type += "_m-"
+    def _get_pokemon_sprite_path(
+        self,
+        pokemon_type: PokemonType,
+        face: Literal["right"] | Literal["left"],
+        is_shiny: bool,
+        frame: Literal[1] | Literal[2],
+    ) -> str:
+        if is_shiny:
+            return f"imgs/pokemons/{pokemon_type.national_no}_{face}_{frame}.png"
         else:
-            self._type += "_"
-
-        return self
-
-    def frames(self) -> tuple[str, str]:
-        return self._url("1"), self._url("2")
-
-    def _url(self, frame: str) -> str:
-        return f"{self._base}/{self._face}{self._type}{frame}.png"
+            return f"imgs/pokemons/{pokemon_type.national_no}_{face}_shiny_{frame}.png"
