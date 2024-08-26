f9cea720ad9fbcb7a8368e0d51b9d1d7eb89614b
2jun0 <soo28819@gmail.com>
2024-08-26 16:04:20 +0900

    feat: pokemon model has form and gender field

--- src/models/pokemon.py
+++ src/models/pokemon.py
@@ -3,6 +3,8 @@ from sqlmodel import Field, SQLModel
 
 from src.models.base import DatedAtMixin
 from src.models.pokemon_type import PokemonType
+from src.pokemons.form import Form
+from src.pokemons.gender import Gender
 
 
 class Pokemon(SQLModel, DatedAtMixin, table=True):
@@ -12,4 +14,6 @@ class Pokemon(SQLModel, DatedAtMixin, table=True):
     id: int | None = Field(default=None, primary_key=True)
     type: PokemonType
     is_shiny: bool = Field(default=False)
+    gender: Gender
+    form: Form | None
     owner_id: int = Field(default=None, foreign_key="user.id")
