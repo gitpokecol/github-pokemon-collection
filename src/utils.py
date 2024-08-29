0e60045fd94a53d0a379d7a2b48d7cb8b48583be
2jun0 <soo28819@gmail.com>
2024-08-29 17:01:41 +0900

    refactor: remove @file_cache

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,5 @@
-import functools
-import inspect
-import pickle
 from bisect import bisect_left
 from itertools import accumulate
-from os import path
-from pathlib import Path
 from random import random
 from typing import Sequence, TypeVar
 
@@ -19,44 +14,3 @@ def weighted_sample(population: Sequence[T], weights: Sequence[float], k: int = 
         index = bisect_left(accum, total * random())
         sampl[index] = population[index]
     return list(sampl.values())
-
-
-def file_cache(filename: str):
-    cache_dir = Path("__cache__")
-    cache_file = cache_dir.joinpath(filename + ".pickle")
-
-    def _file_cache(func):
-        if inspect.iscoroutinefunction(func):
-
-            @functools.wraps(func)
-            async def async_inner_file_cache(*args, **kwargs):
-                if path.exists(cache_file):
-                    with open(cache_file, "rb") as f:
-                        rs = pickle.load(f)
-                else:
-                    rs = await func(*args, **kwargs)
-                    cache_file.parent.mkdir(exist_ok=True)
-                    with open(cache_file, "wb") as f:
-                        pickle.dump(rs, f)
-
-                return rs
-
-            return async_inner_file_cache
-        else:
-
-            @functools.wraps(func)
-            def inner_file_cache(*args, **kwargs):
-                if path.exists(cache_file):
-                    with open(cache_file, "rb") as f:
-                        rs = pickle.load(f)
-                else:
-                    rs = func(*args, **kwargs)
-                    cache_file.parent.mkdir(exist_ok=True)
-                    with open(cache_file, "wb") as f:
-                        pickle.dump(rs, f)
-
-                return rs
-
-        return inner_file_cache
-
-    return _file_cache
