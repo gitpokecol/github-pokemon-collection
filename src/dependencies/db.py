1b8a45d4ed1da3058a6b545c135c5b01a042a0f1
2jun0 <soo28819@gmail.com>
2024-09-05 16:11:29 +0900

    fix: every request commit session on end.

--- src/dependencies/db.py
+++ src/dependencies/db.py
@@ -9,6 +9,7 @@ from src.db import engine
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
+        await session.commit()
 
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
